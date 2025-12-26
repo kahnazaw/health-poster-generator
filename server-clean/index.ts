b8f800eb8675575aa392bb403407d3e304e1e496
kahnazaw <kahnazaw@gmail.com>
2025-12-26 15:01:13 +0300

    Set up basic Express server with a health check

--- server-clean/index.ts
+++ server-clean/index.ts
@@ -1,1 +1,13 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Server is working ✅");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
